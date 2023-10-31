942dfabdbb9846bb8f9034752bd81bb9b5a2548a
Corey <coreyvladesov2023@u.northwestern.edu>
2023-10-31 17:48:04 -0500

    edited the index.js file for functionality

--- models/index.js
+++ models/index.js
@@ -2,11 +2,18 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+})
 
 Post.hasMany(Comment, {
   foreignKey: 'post_id',
   onDelete: 'CASCADE'
 });
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+})
 
 Post.belongsTo(User, {
   foreignKey: 'user_id',
@@ -17,5 +24,9 @@ Comment.belongsTo(User, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+})
 
 module.exports = { User, Comment, Post };
