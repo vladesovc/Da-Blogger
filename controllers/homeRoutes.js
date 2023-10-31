5a991d90882e00c855b86c6d1d219e115583520f
Corey <coreyvladesov2023@u.northwestern.edu>
2023-10-31 17:47:50 -0500

    edited homeroutes code

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -57,21 +57,23 @@ router.get('/post/:id', async (req, res) => {
 
 
 // Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
+router.get('/dashboard', withAuth, async (req, res) => {
     try {
         // Find the logged in user based on the session ID
         const userData = await User.findByPk(req.session.user_id, {
             attributes: { exclude: ['password'] },
-            include: [{ model: Project }],
+            include: [{ model: Post }],
         });
 
         const user = userData.get({ plain: true });
+        console.log(user);
 
-        res.render('profile', {
+        res.render('dashboard', {
             ...user,
             logged_in: true
         });
     } catch (err) {
+        console.log(err);
         res.status(500).json(err);
     }
 });
@@ -79,7 +81,7 @@ router.get('/profile', withAuth, async (req, res) => {
 router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in) {
-        res.redirect('/profile');
+        res.redirect('/dashboard');
         return;
     }
 
